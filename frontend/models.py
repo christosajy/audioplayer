deb032080f0a3aadad2558f7f498584d55b0ce1a
Christo sajy <christosajy24@gmail.com>
2024-03-24 14:48:12 +0530

    24-03-24

--- frontend/models.py
+++ frontend/models.py
@@ -5,3 +5,5 @@ class UsersDb(models.Model):
     Email=models.EmailField(null=True, blank=True)
     Username=models.CharField(null=True, blank=True, max_length=255)
     Password=models.CharField(null=True, blank=True, max_length=255)
+    Mob_No=models.CharField(null=True, blank=True, max_length=255)
+    Profile=models.ImageField(upload_to='profile', null=True, blank=True)
